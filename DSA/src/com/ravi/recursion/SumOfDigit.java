6d6493e34871021c6a28dc3fda1a173fc6cb1c98
rks-singh <rks884088@gmail.com>
2023-08-27 15:03:42 +0530

    SumOfDigit Commmited

--- DSA/src/com/ravi/recursion/SumOfDigit.java
+++ DSA/src/com/ravi/recursion/SumOfDigit.java
@@ -1,5 +1,31 @@
 package com.ravi.recursion;
 
 public class SumOfDigit {
+    public static int getSum(int n) {
+        if (n == 0) {
+            return  0;
+        }
+        return getSum(n/10)+(n%10);
+        // Time Complexity ==> O(d)
+        // Space Complexity ==> O(d).
+    }
+
+    public static int getSumIterative(int n) {
+        int result =0;
+        while (n != 0) {
+            int rem = n%10;
+            result +=rem;
+            n /=10;
+        }
+        return result;
+
+        // Time Complexity ==> O(d)
+        // Space Complexity ==> O(1).
+    }
+    public static void main(String[] args) {
+        System.out.println("Sum of Digit ::"+getSum(386));
+        System.out.println("Sum of Digit ::"+getSum(386));
+    }
+
 
 }
