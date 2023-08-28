1a094b6aa71962e8c6ebb03345d320a251118505
rks-singh <rks884088@gmail.com>
2023-08-28 16:26:24 +0530

    GenerateSubSetOfString

---
+++ DSA/src/com/ravi/recursion/GenerateSubsetOfString.java
@@ -0,0 +1,17 @@
+package com.ravi.recursion;
+
+public class GenerateSubsetOfString {
+    public static void generateSubset(String str, String result, int index) {
+
+        if (index == str.length()) {
+            System.out.println(result);
+            return;
+        }
+        generateSubset(str, result, index + 1);
+        generateSubset(str,result+str.charAt(index),index+1);
+    }
+
+    public static void main(String[] args) {
+        generateSubset("ABC","",0);
+    }
+}
